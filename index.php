fb47d6b9c7051350112fa146130e199c187524df
lance60901 <lance60901@gmail.com>
2016-11-30 16:06:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,22 @@
 //echo URLIsValid("www.dhjafhksdahfhf.com");
 
 $file = 'www.yahoo.com.tw';
-checklink("www.yahoo.com.tw");
-checklink("www.dsjksalflknesfsadf.com");
-function checklink($file){
-    $file_headers = @get_headers($file);
-    if(!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') {
-        $exists = false;
-        echo $file;
-        echo "無此網址";
-    }
-    else {
-        $exists = true;
-        echo $file;
-        echo "有此網址";
-    }
+echo check_url("www.yahoo.com.tw");
+echo "   ";
+echo check_url("www.dsjksalflknesfsadf.com");
+echo "   ";
+echo check_url("www.lanceyang.com");
+function check_url($url) {
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    curl_setopt($ch , CURLOPT_RETURNTRANSFER, 1);
+    $data = curl_exec($ch);
+    $headers = curl_getinfo($ch);
+    curl_close($ch);
+
+    return $headers['http_code'];
 }
 //echo $exists;
 ?>
