4929d2812cc900f35bad9f0da06b12cbd7778045
lance60901 <lance60901@gmail.com>
2016-11-30 15:58:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 //echo URLIsValid("www.google.com");
 //echo URLIsValid("www.dhjafhksdahfhf.com");
 
-$file = 'http://www.domain.com/somefile.jpg';
+$file = 'www.yahoo.com.tw';
 $file_headers = @get_headers($file);
 if(!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') {
     $exists = false;
