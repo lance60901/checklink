db931e19ddbd3974470778a335c91d1ece79eb6d
lance60901 <lance60901@gmail.com>
2016-11-30 16:01:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,20 @@
 //echo URLIsValid("www.dhjafhksdahfhf.com");
 
 $file = 'www.yahoo.com.tw';
-$file_headers = @get_headers($file);
-if(!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') {
-    $exists = false;
+checklink("www.yahoo.com.tw");
+checklink("www.dsjksalflknesfsadf.com");
+function checklink($file){
+    $file_headers = @get_headers($file);
+    if(!$file_headers || $file_headers[0] == 'HTTP/1.1 404 Not Found') {
+        $exists = false;
+        echo $file;
+        echo "無此網址";
+    }
+    else {
+        $exists = true;
+        echo $file;
+        echo "有此網址";
+    }
 }
-else {
-    $exists = true;
-}
-
-echo $exists;
+//echo $exists;
 ?>
