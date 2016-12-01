abfbf455d0dfaacef854272a51a702d97292e8e0
lance60901 <lance60901@gmail.com>
2016-12-01 11:22:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 //echo URLIsValid("www.dhjafhksdahfhf.com");
 
 $file = 'www.yahoo.com.tw';
-echo check_url("http://www.yahoo.com.tw");
+echo check_url("http://www.ris.gov.tw/701");
 echo "   ";
 echo check_url("www.dsjksalflknesfsadf.com");
 echo "   ";
