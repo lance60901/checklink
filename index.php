6d0aab74ec232977d503e32fe306c10990bd44e0
lance60901 <lance60901@gmail.com>
2016-12-01 16:09:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ foreach ($LinkstoSubpages as $pagelinks) {
             //$mainlinkpage = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>'.file_get_contents($temp);
             //echo file_get_contents("http://www.luzhou.ris.ca.ntpc.gov.tw/Site/ActivitySidelightDetial/3319");
             $yo = file_get_contents("http://www.luzhou.ris.ca.ntpc.gov.tw/Site/ActivitySidelightDetial/3319");
-            @$newDom->loadHTML(file_get_contents("http://www.luzhou.ris.ca.ntpc.gov.tw/Site/ActivitySidelightDetial/3319"));
+            $newDom->loadHTML(file_get_contents("http://www.luzhou.ris.ca.ntpc.gov.tw/Site/ActivitySidelightDetial/3319"));
             $finallinks = $newDom->getElementById('site_body_center')->getElementsByTagName('a');
             //@$newDom->loadHTML(file_get_contents($temp));
        }
